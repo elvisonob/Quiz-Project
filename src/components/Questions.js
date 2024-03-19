ba36f15b33f2c4a8a036065a981429cf324c3c07
elvisonob <elvisonob@yahoo.com>
2024-03-19 05:05:26 +0000

    Summary Page included

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,20 +1,38 @@
+import React, { useState } from 'react';
 import QUESTIONS from './../questions';
 import classes from './Questions.module.css';
-import React, { useState } from 'react';
+import SummaryPage from './SummaryPage';
 
 const Questions = () => {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  if (quizIsComplete) {
+    return <SummaryPage />;
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+  const onHandleAnswerClick = (answer) => {
+    setUserAnswers((prevAnswers) => {
+      return [...prevAnswers, answer];
+    });
+  };
+
   return (
     <div className={classes.Quiz}>
       <div className={classes.Questions}>
         <h3>{QUESTIONS[activeQuestionIndex].text}</h3>
       </div>
       <ul className={classes.Answers}>
-        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+        {shuffledAnswers.map((answer) => (
           <li key={answer} className={classes.AnswersList}>
-            <button>{answer}</button>
+            <button onClick={() => onHandleAnswerClick(answer)}>
+              {answer}
+            </button>
           </li>
         ))}
       </ul>
---
+++ src/components/SummaryPage.js
@@ -0,0 +1,12 @@
+import Trophy from './../Trophy completion.jpg';
+
+const SummaryPage = () => {
+  return (
+    <div className="QuizCompletedLogo">
+      <img src={Trophy} alt="" width="20%" height="20%" />
+      <h2>Quiz Completed</h2>
+    </div>
+  );
+};
+
+export default SummaryPage;
